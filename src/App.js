c965c08292e7684e46d035367edfa979cc1c3f30
rose <rosedaily1001@gmail.com>
2022-04-02 06:10:52 +0900

    리액트 기초개념 실습
    - JSX를 사용해서 컴포넌트 작성 (컴포넌트명은 대문자 시작)
    - 컴포넌트에 props를 전달해서 props의 데이터를 객체로 변환해서 컴포넌트로 전달 및 사용
    - 구조분해 할당을 통해 편리하게 props 사용 및 재사용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+function Food(props) {
+  const { fav } = props;
+  return <h3>I like {fav}</h3> 
+}
+
 function App() {
-  return <div>Hello React ?! </div>;
+  return (
+    <div>
+      <h1>Hello React ?! </h1>
+      <Food fav="kimchi"/>
+      <Food fav="ramen"/>
+      <Food fav="samgiopsal"/>
+      <Food fav="chukumi"/>
+    </div>
+  );
 }
 
 export default App;
