8d74c7d1183aea16a7b7b39dfc1fd31cd1a40f06
rose <rosedaily1001@gmail.com>
2022-04-05 07:31:38 +0900

    react-router-dom 설치 및 route활용

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 import './App.css';
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import About from './routes/About';
-import Home from './routes/Home';
 
-function App() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" exact={true} element={<Home/>} />
-        <Route path="/about" element={<About/>} />  
-      </Routes>
-    </HashRouter>
-  );
+class App extends React.Component {
+  state = {
+    isLoading : true,
+    movies : [],
+  };
+
+  getMovies = async () => {
+    const {
+      data : {
+        data: {movies},
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading : false });
+  }
+
+  componentDidMount(){
+    // 영화 데이터 로딩!
+    this.getMovies();
+  }
+
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+                <Movie 
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+                />
+            ))}
+          </div>
+        )}
+      </section>
+    )
+  }
 }
 
 export default App;
