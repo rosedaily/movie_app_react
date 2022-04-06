16f4fa19421583111d06b163301ecd0bb4c523f2
rose <rosedaily1001@gmail.com>
2022-04-07 06:03:23 +0900

    버전다운그레이드 및 css 및 js 파일 생성까지만 해둔 초기 상태

---
+++ src/components/Movie.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Movie.css';
+
+function Movie({title, year, summary, poster, genres}){
+  return ;
+}
+
+export default Movie;
---
+++ src/routes/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Home.css';
+
+class Home extends React.Component {
+  
+}
+
+export default Home;
