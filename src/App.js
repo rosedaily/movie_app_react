90c772059a4cf667a22dfefc1ff3ba5fb9329078
rose <rosedaily1001@gmail.com>
2022-04-14 06:45:35 +0900

    네이버 api활용해서 영화 앱 구동시작,
    - app.js 및 search.js 구성.
    - 오류로 인해 정상동작X

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import {HashRouter, Route} from 'react-router-dom'
+import Home from './routes/Home'
+import Search from './routes/Search';
+import Navigation from './components/Navigation';
 
 function App() {
-  return <div>Hello React ?! </div>;
+  return (
+    <HashRouter>
+      <Navigation/>
+      {/* <Route path='/' exact={true} component={Home}></Route> */}
+      <Route path='/search' exact={true} component={Search}></Route>
+    </HashRouter>
+  );
 }
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Navigation.css';
 
 function Navigation() {
-  return ;
+  return (
+    <div>
+      <h1>여기는 네비게이션</h1>
+    </div>
+  );
 }
 
 export default Navigation;
